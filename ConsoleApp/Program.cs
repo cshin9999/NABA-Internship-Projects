b1beb34383c32e8faddbe83c2dc7f3cfb25858a8
T <T@DESKTOP-L01IG5J>
2021-02-06 14:14:04 -0500

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            string path = Directory.GetCurrentDirectory();
+            using (StreamWriter sw = File.CreateText(path + "//sample.txt"))
+            {
+                sw.WriteLine(firstName);
+                sw.WriteLine(lastName);
+            }
+
+            try
+            {
+                using (var sr = new StreamReader("sample.txt"))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
